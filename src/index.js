9b5131b0c8761584ce942523985676c91fe0b90e
Luiz Calaça <luizcalaca@gmail.com>
2022-07-02 11:17:11 -0300

    feat: template for upload Node.js to S3 AWS

---
+++ src/index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const cors = require("cors");
+const routes = require('../src/routes/routes')
+require('./helpers/connection')
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+);
+
+app.use(routes);
+
+app.listen(3000);
